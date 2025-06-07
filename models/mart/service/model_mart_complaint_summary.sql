92fac4722e44bbc53720abbc134de92782c7c216
Jiraporn-C <jiraporn.cha98@gmail.com>
2025-06-07 22:03:39 +0700

    add yml for mart layer

--- models/mart/service/model_mart_complaint_summary.sql
+++ models/mart/service/model_mart_complaint_summary.sql
@@ -5,7 +5,7 @@ select
   date_trunc('day',opened_datetime) as complaint_date,
   count(distinct ticket_id) as total_complaints,
   sum(compensation_amount) as total_compensation,
-  avg(resolution_duration_mins) as avg_resolution_time,
+  round(avg(resolution_duration_mins),2) as avg_resolution_time,
   sum(is_resolved) as total_resolved,
 from {{ ref('model_int_ticket_resolution_summary') }}
 group by 1, 2
